49efb62cdce1c80bc03f0242cfee3d7948d771c7
Mahipal Choudhary <mschoudhary81@gmail.com>
2022-08-05 15:24:16 +0530

    Update sidebar and navigation menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,31 @@
+import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component } from '@angular/core';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Fruit',
+    children: [{ name: 'Apple' }, { name: 'Banana' }, { name: 'Fruit loops' }],
+  },
+  {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [{ name: 'Broccoli' }, { name: 'Brussels sprouts' }],
+      },
+      {
+        name: 'Orange',
+        children: [{ name: 'Pumpkins' }, { name: 'Carrots' }],
+      },
+    ],
+  },
+];
 
 @Component({
   selector: 'app-root',
@@ -8,4 +35,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularMaterialTemplate';
   IsDrawer: boolean = true;
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+
 }
