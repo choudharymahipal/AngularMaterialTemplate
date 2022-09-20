e9551665f428f66a2d3a36f5919b0fa519a00da2
Mahipal Choudhary <mschoudhary81@gmail.com>
2022-09-20 14:13:30 +0530

    Deploy on GitHub Pages

--- src/app/shared/components/inbox/inbox.component.ts
+++ src/app/shared/components/inbox/inbox.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'mahi-inbox',
   templateUrl: './inbox.component.html',
   styleUrls: ['./inbox.component.scss']
 })
-export class InboxComponent implements OnInit {
+export class InboxComponent implements OnInit, AfterViewChecked {
+  selectedConversation: boolean = true;
+  form!: FormGroup;
+  IamOnline:boolean=true;
 
-  constructor() { }
+  @ViewChild('scrollMe') private myScrollContainer!: ElementRef;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      input: [null]
+    });
+  }
 
   ngOnInit(): void {
+
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) { }
+  }
+
+  ngAfterViewChecked() {
+
+  }
+
+  onSubmit() {
+    this.scrollToBottom();
   }
 
 }
